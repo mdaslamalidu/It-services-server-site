b91db001d3bf55879a4bff9465ad8ac8d51d2de5
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-25 21:17:30 +0600

    added signleCourse

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
-const courses = require("./data/classes.json");
+const courses = require("./data/courses.json");
 const category = require("./data/category.json");
 
 
@@ -31,6 +31,13 @@ app.get("/courses", (req,res) => {
     res.send(courses)
 })
 
+app.get("/singleCourse/:id", (req,res) => {
+    const id = req.params.id;
+    const selectedId = courses.find(course => course._id === id);
+    res.send(selectedId)
+})
+
+app.get("/")
 
 app.listen(port, () => {
     console.log(`website is running on port ${port}`)
